7eb392405677f23e24b69562c94e47cbaac4063b
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-06-03 15:03:57 -0700

    Fix Deployment Bug

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
