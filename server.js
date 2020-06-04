9809dd3501840401adf2f6bbacc7364fd3de1661
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-06-03 17:15:54 -0700

    Mongo Connect

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const Url = require("./src/url");
+const connect = require("./src/connect")
+const app = express();
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+
+
+connect(`mongodb://localhost:27017/url-shortener`)
+.then(()=> app.listen(PORT, () => console.log(`server on http://localhost:${PORT}`)))
+.catch(error => console.error(error))
