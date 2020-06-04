9033016e94ec2381e9169e49b53f8af9bbf1efb0
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-06-03 19:56:47 -0700

    Schemas Created

--- src/url.js
+++ src/url.js
@@ -1,3 +1,20 @@
 const mongoose = require("mongoose");
 
+const urlSchema = new mongoose.Schema({
+    longUrl:{
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        unique: true
+    },
+    timesUsed: Number,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "user"
+    }
+}, {timestamps: true})
+
 module.exports = mongoose.model("url", urlSchema);
